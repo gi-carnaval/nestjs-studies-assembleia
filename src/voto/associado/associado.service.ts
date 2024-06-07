49527e85d86daaac9d5d6edd29d3643a7333d10a
Giovani Carnaval <gi.carnaval91@gmail.com>
2024-06-07 02:41:03 -0300

    feat: Developing associados services

--- src/voto/associado/associado.service.ts
+++ src/voto/associado/associado.service.ts
@@ -8,4 +8,24 @@ export class AssociadoService {
     @Inject('ASSOCIADO_REPOSITORY')
     private readonly associadoRepository: Repository<Associado>,
   ) {}
+
+  async obterPorCpf(cpf: string): Promise<Associado> {
+    return await this.associadoRepository.findOne({
+      where: { cpf: cpf },
+    });
+  }
+
+  async recuperarOuCadastrar(cpf: string): Promise<Associado> {
+    const associadoEncontrado: Associado = await this.obterPorCpf(cpf);
+
+    if (associadoEncontrado) {
+      return associadoEncontrado;
+    }
+
+    const novoAssociado = new Associado();
+    novoAssociado.cpf = cpf;
+
+    await this.associadoRepository.save(novoAssociado);
+    return novoAssociado;
+  }
 }
