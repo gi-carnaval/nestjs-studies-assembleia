446ba0154c79eba9baa97acf7915f8243e635209
Giovani Carnaval <gi.carnaval91@gmail.com>
2024-06-09 17:16:59 -0300

    core: Adding validator and swagger to nestJs

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,27 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(new ValidationPipe());
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Assembleia API')
+    .setDescription('API do projeto Assembleia')
+    .setVersion('1.0.0')
+    .setContact(
+      'Giovani Carnaval',
+      'https://github.com/gi-carnaval',
+      'gi.carnaval@hotmail.com',
+    )
+    .build();
+
+  const swaggerDoc = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api', app, swaggerDoc);
+
   await app.listen(3000);
 }
 bootstrap();
